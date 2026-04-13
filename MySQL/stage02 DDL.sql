6805b47ddb715fac7244eef54cd43eba1a886cbe
Bienvenu Myriel <2801605948@qq.com>
2026-04-13 22:27:40 +0800

    Update stage02 DDL.sql

--- MySQL/stage02 DDL.sql
+++ MySQL/stage02 DDL.sql
@@ -1,1 +1,13 @@
+[]中括号内的内容表示可加可不加
+DDL表单的修改和删除:
+增加表单field(字段)的方法： ALTER TABLE HUAHUA ADD NAME2 VARCHAR(6) [COMMENT 'SAO'];   --HUAHUA是已有表单的名字（在这个表单里增加一个FIELD（字段）） ADD后面加自定义的名字（就是字段） +数据类型 
+
+修改表单ytpe的方法：ALTER TABLE HUAHUA MODIFY NAME2 INT COMMENT '这是修改数据类型（type）的方法';   --MODIFY是关键词 int要变更成的数据类型（会覆盖原来的数据类型）
+
+修改表单里字段的方法：ALTER TABLE HUAHUA CHANGE NAME2 NEW_NAME3 VARCHAR(10) COMMENT '这个方法可以同时修改Field和Type'; --NAME2就是旧的字段名 NEW_NAME3就是新的字段名，VARCHAR(10)是新的数据类型，
+NEW_NAME3和VARCHAR(10)都会覆盖之前的数据。
+
+删除字段的方法：ALTER TABLE HUAHUA DROP SAO;  --SAO就是字段名
+
+重命名表单（table）的方法：ALTER TABLE HUAHUA RENAME TO MUJICA; --把表单（TABLE）huahua改名成mujica。
 
