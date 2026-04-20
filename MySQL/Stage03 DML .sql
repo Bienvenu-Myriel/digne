2476c6092b4ed26f33cbb9fbdf352bac3d47f252
Bienvenu Myriel <2801605948@qq.com>
2026-04-20 21:20:07 +0800

    Update Stage03 DML .sql

--- MySQL/Stage03 DML .sql
+++ MySQL/Stage03 DML .sql
@@ -1,6 +1,7 @@
 DML添加数据：
 1.给指定字段添加数据：
 INSERT INTO 表名(字段1,字段2,...) VALUES(值1,值2,...);
+实践：  INSERT INTO MUJICA(ID, NAME, AGE, GENDER, NEW_NAME3)  VALUES(01,'花花',18,'男','花仙子')
 
 
 2.给全部字段添加数据：
@@ -12,4 +13,4 @@ INSERT INTO 表名(字段名1,字段名2,...) VALUES(值1,值2,...),(值1,值2,.
 INSERT INTO 表名VALUES(值1,值2,...),(值1,值2,...),(值1,值2,...);
 
 
-
+SELECT * FROM MUJICA   --从 MUJICA 这张表中，查询所有列、所有行的数据
