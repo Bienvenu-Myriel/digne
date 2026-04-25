2ea3c8e91b14e9eb902b86f3dc0a4b6ddb518607
Bienvenu Myriel <2801605948@qq.com>
2026-04-26 02:09:12 +0800

    Update Stage04 DQL .sql

--- MySQL/Stage04 DQL .sql
+++ MySQL/Stage04 DQL .sql
@@ -2,5 +2,14 @@
 
 查询多个字段：
 SELECT 字段1,字段2,字段3...FROM 表名;
+例：SELECT NAME,ID,AGE FROM MUJICA;  -- 相当于把表切片，只查看这部分字段的表
 查询整个表：
 SELECT*FROM 表名;
+
+设置别名
+SELECT 字段1 [AS 别名1],字段2[AS 别名2]...FROM 表名;
+
+去除重复记录：
+SELECT DISTINCT 字段列表 FROM 表名;
+
+
