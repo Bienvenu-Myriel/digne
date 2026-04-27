e8be9e7eb9d633dd62437c4008ce1278239367cb
Bienvenu Myriel <2801605948@qq.com>
2026-04-27 15:48:07 +0800

    Update Stage04 DQL.sql

--- MySQL/Stage04 DQL.sql
+++ MySQL/Stage04 DQL.sql
@@ -24,23 +24,23 @@ SELECT DISTINCT NAME  '名字' FROM MUJICA; -- 可以和设置别名组合
 #例1：
 SELECT * FROM MUJICA WHERE age > 20;
 #例2:
-SELECT * FROM MUJICA WHERE age BETWEEN 20 AND 40; -- 查询age位于20到40的字段
+SELECT * FROM MUJICA WHERE age BETWEEN 20 AND 40; -- 查询age位于20到40的数据
 SELECT * FROM MUJICA WHERE age >= 20 && age <= 40; -- 和BETWEEN ... AND ... 是等效的，把&&替换成and效果一样。
 #例3：
-SELECT * FROM MUJICA WHERE NEW_NAME3 IS NULL;  -- 查询NEW_NAME3为null的字段
+SELECT * FROM MUJICA WHERE NEW_NAME3 IS NULL;  -- 查询NEW_NAME3为null的数据
 SELECT * FROM MUJICA WHERE NEW_NAME3 IS NOT NULL;  -- 不用多说了吧？
 #例4：
-SELECT * FROM MUJICA WHERE AGE IN (20,35,28);  -- 查询()内有的字段
+SELECT * FROM MUJICA WHERE AGE IN (20,35,28);  -- 查询()内有的数据
 #例5：
 SELECT * FROM MUJICA WHERE NAME LIKE '___';  -- ''里一共三个下划线，一个_对应一个字符,'_花'，第一个随意字符，但第二个必须是花
-SELECT * FROM MUJICA WHERE NAME LIKE '花%';  -- %表示任意字符数，但是“花”必须在首位，'%花'就是花必须在末尾，'%花%'就是所有包含花的字段
+SELECT * FROM MUJICA WHERE NAME LIKE '花%';  -- %表示任意字符数，但是“花”必须在首位，'%花'就是花必须在末尾，'%花%'就是所有包含花的数据
 
 -- 聚合函数（null值不参与）,复合函数参考Stage04 DQL+.sql
 #格式：
 SELECT 聚合函数() FROM 表名;
 #例1：
-SELECT COUNT(*) FROM MUJICA; -- *是全体，就是在计算一共有多少条字段
-SELECT COUNT(ID) FROM MUJICA; -- 因为null值不参与，所有不会计算包含有null值的字段
+SELECT COUNT(*) FROM MUJICA; -- *是全体，就是在计算一共有多少条数据
+SELECT COUNT(ID) FROM MUJICA; -- 因为null值不参与，所有不会计算包含有null值的数据
 #例2：
 SELECT AVG(AGE) FROM MUJICA; -- 计算AGE的平均，avg就是average
 #例3：
