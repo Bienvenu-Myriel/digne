f11e38aeb9f6204e7cb63d51780886c872bd754c
Bienvenu Myriel <2801605948@qq.com>
2026-04-28 15:35:07 +0800

    Update Stage05 DQL.sql

--- MySQL/Stage05 DQL.sql
+++ MySQL/Stage05 DQL.sql
@@ -1,5 +1,9 @@
 -- 分组查询：
 #语法：
-SELECT 字段列表 FROM 表名 [WHERE 条件] GROUP BY 分组字段名 [HAVING 分组后过滤条件]; 
+SELECT 字段列表1,字段2（也可以是聚合函数）,... FROM 表名 [WHERE 条件] GROUP BY 分组字段名 [HAVING 分组后过滤条件];  
 -- where和having不同，where在分组之前进行过滤，不满足where条件，不参与分组，having则是分组后对结果过滤，另外having可以对聚合函数进行判断而where不行。
 #例1：
+SELECT AGE,COUNT(*) FROM MUJICA GROUP BY AGE; -- COUNT(*)的作用就是COUNT(*) = 统计行数
+#例2：
+SELECT AGE,AVG(AGE),COUNT(*) FROM MUJICA GROUP BY AGE; -- COUNT(*)可以不加，它的作用就是多一列显示不同年龄段各有多少。
+#例3：
