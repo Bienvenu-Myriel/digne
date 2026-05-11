bd0a9c1d00063a1baab240b044dbb6994fa250ad
Bienvenu Myriel <2801605948@qq.com>
2026-05-11 13:00:38 +0800

    Update Stage05 part 2 DQLworksheet.sql

--- MySQL/Stage05 part 2 DQLworksheet.sql
+++ MySQL/Stage05 part 2 DQLworksheet.sql
@@ -2,4 +2,6 @@
 SELECT * FROM mujica WHERE AGE IN (18,19,20) AND GENDER ='男';
 
 -- 2.查询不同性别，年龄小于30岁，而且名字为三个字的人。
+SELECT GENDER, COUNT(*) FROM MUJICA WHERE AGE < 30 AND NAME LIKE '___' GROUP BY GENDER;  -- 注意检索 聚合函数 + 普通字段（列） → 必须加 GROUP BY
 
+-- 3.
