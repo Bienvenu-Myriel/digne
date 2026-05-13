e70ab3d2db0f05419fdb859e85e42686c0d73ea8
Bienvenu Myriel <2801605948@qq.com>
2026-05-13 20:15:47 +0800

    Create Stage01 java.js

---
+++ front-end technnologies/Stage01 java.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScript</title>
+    <style>
+        .zeus {
+            width: 200px;   height: 200px;
+            background-color: red;
+        }
+    </style>
+</head>
+<body>
+   
+    <div class="zeus" onclick="alert('do not touch me')"></div>
+    <script>
+        document.querySelector('.zeus').addEventListener('click', function() {
+            alert('do not touch me');
+        });
+    </script>
+
+    <script>
+        let zeus = 0
+        let gumayusi = 1
+        let chovy = 2
+        let keria = 'char'
+        let faker = '花'
+        document.write('zeus: ' + zeus + '<br>')
+        document.write(`guamyusi_类型为:${typeof gumayusi}` + '<br>');
+        document.write(`chovy_类型为:${typeof chovy}` + '<br>');
+        document.write(`keria_类型为:${typeof keria}` + '<br>');
+        document.write(`faker_类型为:${typeof faker}` + '<br>');
+        // $符号和python里的f字符串类似，可以在字符串里直接使用变量，前提是字符串要用反引号（`）包裹起来
+    </script>
+</body>
+</html>
