11a3655037ef4a4c9b72596c1ad5c88271d00e83
Bienvenu Myriel <2801605948@qq.com>
2026-05-19 19:58:20 +0800

    Create Stage01 javascript.js

---
+++ front-end technnologies/Stage01 javascript.js
@@ -0,0 +1,77 @@
+<script>
+  let num = +prompt("输入rating");
+  num = Math.round(num);
+  if(isNanN(NUM)){
+    alert("请输入rating")
+  }
+  else if (num >= 1.20 && num <= 1.50){
+    alert("你是top1有力竞争者")
+  }
+  else if(num >= 1.10 && num <= 1.20){
+    alert("你是高位top")
+  }
+  else (num >= 1.00 && num <= 1.10){
+    alert("你是低位top")
+  }
+ <script> 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  )
