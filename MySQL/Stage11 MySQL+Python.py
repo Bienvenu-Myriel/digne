48982ba7c2b10284aab902dd6fe14199dadad3f3
Bienvenu Myriel <2801605948@qq.com>
2026-05-25 19:58:05 +0800

    Update Stage11 MySQL+Python.py

--- MySQL/Stage11 MySQL+Python.py
+++ MySQL/Stage11 MySQL+Python.py
@@ -1,6 +1,6 @@
 import pymysql
 
-#Obtain the connection object of MySQL database(获取到MySQL数据库的链接对象)
+# Obtain the connection object of MySQL database(获取到MySQL数据库的链接对象)
 conn = pymysql.connect(
     host='localhost',   # localhost or ip
     port=3306,          # 3306 is default port(默认端口)
@@ -10,18 +10,26 @@ conn = pymysql.connect(
     charset='utf8mb4'
 )
 
-cur = conn.cursor() #Get cursor object(获取游标对象)
+# Get cursor object(获取游标对象)
+cur = conn.cursor()
 
-cur.execute('select * from player')  #Execute SQL statement with cursor object(使用游标对象执行sql语句（可以不写;）)
+# Execute SQL statement with cursor object(使用游标对象执行sql语句（可以不写;）)
+cur.execute('select * from player')
 
 # Obtain query results
 results:tuple = cur.fetchall()
-#fetchall = fetch all records(获取所有查询结果)
+# fetchall = fetch all records(获取所有查询结果)
 # fetchone = fetch one record
 
 for r in results:
     print(r)
 
+# require commit confirmation (需要提交确认)
+conn.commit()
+
 # Close connection (关闭数据库连接)
 cur.close()
 conn.close()
+
+# ======================================
+也可以在构建数据库连接对象的时候增加一个autocommit = true的属性 (默认为false)
