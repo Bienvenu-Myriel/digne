d540bbbedaa1c96f917c680aa0e87c1d09ca3cc0
Bienvenu Myriel <2801605948@qq.com>
2026-05-26 19:26:36 +0800

    Create stage02 .js

---
+++ front-end technnologies/stage02 .js
@@ -0,0 +1,18 @@
+<script>
+function add(a,b) {
+  return a + b
+}
+console.log(`a+b的结果为：${add(1,2)}`)
+
+  //匿名函数
+  let mul = function (a,b){
+    return a*b
+  }
+    console.log(`a*b的结果为：${mu;(2,3)}`)
+
+//箭头函数，vue前端框架
+  let div = (a,b) => a/b
+  console.log(`a/b的结果为：${div(4,2)}`)
+</script>
+
+
